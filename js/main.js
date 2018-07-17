651401a825b330c81b0099ddee7fc57733f740f5
Jessica-Small <jlsmall@ufl.edu>
2018-07-16 22:33:53 -0400

    revise headings function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   var $headings = $('h2');
 
-  $headings.text("I’m Jessica Small, and I’m learning jQuery!");
+  $headings.last().text("I’m Jessica Small, and I’m learning jQuery!");
 });
